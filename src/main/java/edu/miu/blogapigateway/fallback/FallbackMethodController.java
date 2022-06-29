836a7449fdd97db6deda0ca1186f67f926631555
Sagar <worthless.dumboo@gmail.com>
2022-06-28 23:27:42 -0500

    Keycloak added with errors

--- src/main/java/edu/miu/blogapigateway/fallback/FallbackMethodController.java
+++ src/main/java/edu/miu/blogapigateway/fallback/FallbackMethodController.java
@@ -3,6 +3,8 @@ package edu.miu.blogapigateway.fallback;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
+
 @RestController
 public class FallbackMethodController {
 
